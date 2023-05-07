51d782bc2155e99d31100b3504fac1866014404f
roshan <roshan.budahthoki@gmail.com>
2023-05-07 09:12:18 +0545

    adding question numeric section

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../shared/Header/Header'
 import Footer from '../shared/Footer/Footer'
 import HeroSection from './HeroSection'
+import WhyNumberic from './WhyNumeric'
 
 const Home = () => {
   return (
     <>
         <Header />
         <HeroSection />
+        <WhyNumberic />
         <Footer />
     </>
   )
