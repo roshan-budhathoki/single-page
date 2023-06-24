55934cf30b7658019f787cf8e60cd888e2f0ed94
roshan <roshan.budahthoki@gmail.com>
2023-06-24 23:39:31 +0545

    carrer page added

--- src/components/career/CareerContent.jsx
+++ src/components/career/CareerContent.jsx
@@ -64,35 +64,45 @@ const CareerContent = () => {
                 </Box>
             </Box>
             <Container>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} marginBottom="4rem" marginTop="1.5rem!important">
                 {
-                    careerNumeric.map((career, index) => 
-                    <Grid item xs={12} sm={12} md={4} key={index} 
-                        sx={{
-                                marginTop: "1rem",
-                                marginBottom: ".5rem"
-                            }}
-                    >
-                        <Box sx={{maxWidth: "20rem", 
-                            height: "100%", 
-                            boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                            background: "#fff", borderRadius: "1rem",  paddingX: "2rem", paddingTop:"1.25rem"}}>
-                            <Typography fontWeight="600" fontSize={{ md: "1.5rem", sm: "1rem", xs: "1rem" }} fontFamily="Poppins" color="#1F284F">
-                                {career.jobOpening.split("/")[0]}
-                            </Typography>
-                            <Typography fontWeight="600" fontSize={{ md: "1.5rem", sm: "1rem", xs: "1rem" }} fontFamily="Poppins" color="#1F284F">
-                                {`/${career.jobOpening.split("/")[1]}`}
-                            </Typography>
-                            <Box borderLeft="3px solid #1F284F"  color="#0070F0E5" paddingLeft="1rem" marginTop="1rem">
-                                <Link href={career.url} sx={{textDecoration: "none"}}>
-                                    View Job
-                                </Link>
+
+                    false && 
+
+                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} marginBottom="4rem" marginTop="1.5rem!important">
+                    {
+                        careerNumeric.map((career, index) => 
+                        <Grid item xs={12} sm={12} md={4} key={index} 
+                            sx={{
+                                    marginTop: "1rem",
+                                    marginBottom: ".5rem"
+                                }}
+                        >
+                            <Box sx={{maxWidth: "20rem", 
+                                height: "100%", 
+                                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                                background: "#fff", borderRadius: "1rem",  paddingX: "2rem", paddingTop:"1.25rem"}}>
+                                <Typography fontWeight="600" fontSize={{ md: "1.5rem", sm: "1rem", xs: "1rem" }} fontFamily="Poppins" color="#1F284F">
+                                    {career.jobOpening.split("/")[0]}
+                                </Typography>
+                                <Typography fontWeight="600" fontSize={{ md: "1.5rem", sm: "1rem", xs: "1rem" }} fontFamily="Poppins" color="#1F284F">
+                                    {`/${career.jobOpening.split("/")[1]}`}
+                                </Typography>
+                                <Box borderLeft="3px solid #1F284F"  color="#0070F0E5" paddingLeft="1rem" marginTop="1rem">
+                                    <Link href={career.url} sx={{textDecoration: "none"}}>
+                                        View Job
+                                    </Link>
+                                </Box>
                             </Box>
-                        </Box>
-                    </Grid>
-                    )
+                        </Grid>
+                        )
+                    }
+                   </Grid>
                 }
-                </Grid>
+                <Box width="100%" display="flex" height="15rem" justifyContent="center" alignContent="center" alignItems="center">
+                    <Typography fontWeight="600" fontSize="2rem" color="#1F222A">
+                        There is no current opening.
+                    </Typography>
+                </Box>
             </Container>
         </Container>
     )
